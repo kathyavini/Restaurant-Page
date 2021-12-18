1895764f2c6efc456a1791078ca28e8f761c1922
kathyavini <kathyavini@gmail.com>
2021-12-18 01:10:43 -0800

    Refine styles and add menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
-import './loader.css';
+import './loading-io.css';
 // import pageLoad from './initialPageLoad';
+
 
 let background = document.querySelector('.background');
 let loader = document.querySelector('.loader');
@@ -16,13 +17,12 @@ window.addEventListener('load', () => {
     }, 2000);
 
 })
-// pageLoad();
 
 let container = document.querySelector('#content');
 
 let startButton = document.querySelector('button');
 let punjabi = document.querySelector('.punjabi');
-let english = document.querySelectorAll('.english');
+let english = document.querySelector('.english');
 let skipButton = document.querySelector('.skip');
 
 startButton.addEventListener('click', () => {
@@ -33,11 +33,10 @@ startButton.addEventListener('click', () => {
 })
 
 skipButton.addEventListener('click', () => {
+    english.classList.add('quick');
     makeFinalState();
     skipButton.style.opacity = '0';
 })
-
-// let smallImage = document.createElement("div");
 
 function changeImage() {
     background.classList.remove('transformed');
@@ -45,41 +44,108 @@ function changeImage() {
     background.classList.add('transformed');
     skipButton.style.opacity = '0';
 
-
-    // punjabi.style.opacity = '1';
-
-    // smallImage.classList.add('small-image');    
-    // container.appendChild(smallImage);
-    // smallImage.classList.add('visible');
-
     background.removeEventListener('transitionend', changeImage);
+    
     setTimeout(startVideo, 2000);
-    // background.addEventListener('transitionstart', startVideo);
 }
 
+let video = document.createElement("video");
 function startVideo() {
-    let video = document.createElement("video");
     video.setAttribute('src', "../src/img/chai.mp4#t=2")
     video.setAttribute('autoplay', "true");
     video.setAttribute('muted', "true");
     container.appendChild(video);
     video.classList.add('visible');
     punjabi.classList.add('visible');
-
     video.addEventListener('ended', () => {
-        video.classList.remove('visible');
         makeFinalState();
-
+    })
+    video.addEventListener('paused', () => {
+        console.log("Video paused")
+        makeFinalState();
     })
 }
 
+
+
 function makeFinalState() {
+    video.classList.remove('visible');
     punjabi.classList.add('transitioned');
-    english.forEach(element =>
-        element.classList.add('transitioned')
-    )
+    english.classList.add('transitioned');
     background.classList.remove('transformed');
     background.classList.remove('second-image');
     background.classList.add('final');
     navBar.classList.add('visible');
+    introTab.classList.add('current');
+}
+
+let introTab = document.querySelector('#intro');
+introTab.addEventListener('click', () => {
+    makeInitialState();
+});
+
+function makeInitialState() {
+    clearMenu();
+    clearAbout();
+    startButton.pointerEvents = 'initial';
+    english.style.visibility = 'visible';
+    punjabi.classList.remove('visible');
+    punjabi.classList.remove('transitioned');
+    english.classList.remove('transitioned');
+    english.classList.remove('quick');
+    background.classList.remove('transformed');
+    background.classList.remove('second-image');
+    background.classList.remove('final');
+    navBar.classList.remove('visible');
+    startButton.style.opacity = '1';
+}
+
+
+
+// Menu functionality
+let menuTab = document.querySelector('#menu');
+menuTab.addEventListener('click', () => {
+    clearMain();
+    showMenu();
+});
+
+function clearMain() {
+    english.style.visibility = 'hidden';
+    introTab.classList.remove('current');
+}
+
+let menu = document.querySelector('.menu');
+function showMenu() {
+    menuTab.classList.add('current');
+    startButton.pointerEvents = 'none';
+    menu.classList.add('onLoad');
+    menu.classList.add('show');
+}
+function clearMenu() {
+    menu.classList.remove('onLoad');
+    menu.classList.remove('show');
+    menuTab.classList.remove('current');
+}
+
+// About tab
+let aboutTab = document.querySelector('#about');
+aboutTab.addEventListener('click', () => {
+    clearMain();
+    clearMenu();
+    showAbout();
+});
+
+let about = document.querySelector('.about');
+function showAbout() {
+    aboutTab.classList.add('current');
+    startButton.pointerEvents = 'none';
+    // about.classList.remove('onLoad');
+    about.classList.add('show');
+}
+
+function clearAbout() {
+    aboutTab.classList.remove('current');
+    // about.classList.remove('onLoad');
+    about.classList.remove('show');
+
 }
